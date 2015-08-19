830e156bd551f21043d855d8045f968ff84eedc0
Hyunho Ha <hyunhoha@eduroam-169-233-220-118.ucsc.edu>
2015-08-20 08:50:27 +0900

    day hour heat map

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -3,8 +3,10 @@
 collectData(function() {
   console.log(domainCount);
   console.log(dateCount);
+  console.log(dayHourCount);
   drawHitsPerDay(dateCount);
   drawDomainsRank(domainCount);
+  drawDayHourHeatMap(dayHourCount);
 });
 
 function drawDomainsRank (data) {
@@ -69,6 +71,7 @@ function drawHitsPerDay (data) {
 
 }
 
+<<<<<<< HEAD
 
 //get random color for piechart?
 function getRandomColor() {
@@ -108,7 +111,7 @@ function drawPieChart(){
   },
   "data": {
     "sortOrder": "value-desc",
-    "content": b,    
+    "content": b,
     "labels": {
     "inner": {
     "hideWhenLessThanPercentage": 3
@@ -153,3 +156,82 @@ function drawPieChart(){
 
 
 
+=======
+function drawDayHourHeatMap (data) {
+	var margin = { top: 50, right: 0, bottom: 100, left: 30 },
+		width = 960 - margin.left - margin.right,
+		height = 430 - margin.top - margin.bottom,
+		gridSize = Math.floor(width / 24),
+		legendElementWidth = gridSize*2,
+		buckets = 9,
+		colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+		days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+		times = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"];
+
+
+	var colorScale = d3.scale.quantile()
+		.domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
+		.range(colors);
+
+	var svg = d3.select("#dayHourHeats").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var dayLabels = svg.selectAll(".dayLabel")
+		.data(days)
+		.enter().append("text")
+		.text(function (d) { return d; })
+		.attr("x", 0)
+		.attr("y", function (d, i) { return i * gridSize; })
+		.style("text-anchor", "end")
+		.attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+		.attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+
+	var timeLabels = svg.selectAll(".timeLabel")
+		.data(times)
+		.enter().append("text")
+		.text(function(d) { return d; })
+		.attr("x", function(d, i) { return i * gridSize; })
+		.attr("y", 0)
+		.style("text-anchor", "middle")
+		.attr("transform", "translate(" + gridSize / 2 + ", -6)")
+		.attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+
+	var heatMap = svg.selectAll(".hour")
+		.data(data)
+		.enter().append("rect")
+		.attr("x", function(d) { return (d.hour - 1) * gridSize; })
+		.attr("y", function(d) { return (d.day - 1) * gridSize; })
+		.attr("rx", 4)
+		.attr("ry", 4)
+		.attr("class", "hour bordered")
+		.attr("width", gridSize)
+		.attr("height", gridSize)
+		.style("fill", colors[0]);
+
+	heatMap.transition().duration(1000)
+		.style("fill", function(d) { return colorScale(d.value); });
+
+	heatMap.append("title").text(function(d) { return d.value; });
+
+	var legend = svg.selectAll(".legend")
+		.data([0].concat(colorScale.quantiles()), function(d) { return d; })
+		.enter().append("g")
+		.attr("class", "legend");
+
+	legend.append("rect")
+		.attr("x", function(d, i) { return legendElementWidth * i; })
+		.attr("y", height)
+		.attr("width", legendElementWidth)
+		.attr("height", gridSize / 2)
+		.style("fill", function(d, i) { return colors[i]; });
+
+	legend.append("text")
+		.attr("class", "mono")
+		.text(function(d) { return "" + Math.round(d); })
+		.attr("x", function(d, i) { return legendElementWidth * i; })
+		.attr("y", height + gridSize);
+}
+>>>>>>> day hour heat map
