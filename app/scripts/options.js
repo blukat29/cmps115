263dbb8de27332e10cc7c5b7884f31a16d9db16d
tburch7 <tburch1@ucsc.edu>
2015-08-20 10:09:49 -0700

    Piechart Finalized

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -5,6 +5,7 @@ collectData(function() {
   console.log(dateCount);
   drawHitsPerDay(dateCount);
   drawDomainsRank(domainCount);
+  drawpieChart();
 });
 
 function drawDomainsRank (data) {
@@ -69,7 +70,6 @@ function drawHitsPerDay (data) {
 
 }
 
-/*
 //get random color for piechart?
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
@@ -80,23 +80,20 @@ function getRandomColor() {
     return color;
 }
 
-function getpieData(){
-  var pieData = [];
-  for (var i=0; i<domainCount.length; ++i){
+function drawpieChart(){
+var pieData = [];
+  for(var i = 0; i < domainCount.length; ++i){
     pieData.push({
-        "label": domainCount.domain[i],
-        "value": domainCount.count[i],
-        "color": getRandomColor()
-      });
-  } 
-}
-
-function drawPieChart(){
+    "label": domainCount[i].domain,
+    "value": domainCount[i].count,
+    "color": getRandomColor()
+  });
+  }
 
 var pie = new d3pie("pieChart", {
   "header": {
     "title": {
-      "text": "Top hits domains",
+      "text": "Domain Count",
       "fontSize": 24,
       "font": "open sans"
     },
@@ -105,7 +102,6 @@ var pie = new d3pie("pieChart", {
       "fontSize": 12,
       "font": "open sans"
     },
-    "location": "top-left",
     "titleSubtitlePadding": 9
   },
   "footer": {
@@ -120,11 +116,7 @@ var pie = new d3pie("pieChart", {
   },
   "data": {
     "sortOrder": "value-desc",
-    "content": [  
-        "label": domainCount.domain[0],
-        "value": domainCount.count[0],
-        "color": #000FFF
-    ]
+    "content": pieData
   },
   "labels": {
     "outer": {
@@ -154,8 +146,8 @@ var pie = new d3pie("pieChart", {
   "effects": {
     "pullOutSegmentOnClick": {
       "effect": "linear",
-      "speed": 400,
-      "size": 8
+      "speed": 500,
+      "size": 10
     }
   },
   "misc": {
@@ -169,11 +161,6 @@ var pie = new d3pie("pieChart", {
   },
   "callbacks": {}
 });
-}
-*/
-
-function drawPieChart(){
-var pie = new d3pie("pieChart", {"header":{"title":{"text":"Lots of Programming Languages","fontSize":24,"font":"open sans"},"subtitle":{"text":"A full pie chart to show off label collision detection and resolution.","color":"#999999","fontSize":12,"font":"open sans"},"titleSubtitlePadding":9},"footer":{"color":"#999999","fontSize":10,"font":"open sans","location":"bottom-left"},"size":{"canvasWidth":590,"pieOuterRadius":"90%"},"data":{"sortOrder":"value-desc","content":[{"label":"JavaScript","value":264131,"color":"#2484c1"},{"label":"Ruby","value":218812,"color":"#0c6197"},{"label":"Java","value":157618,"color":"#4daa4b"},{"label":"PHP","value":114384,"color":"#90c469"},{"label":"Python","value":95002,"color":"#daca61"},{"label":"C+","value":78327,"color":"#e4a14b"},{"label":"C","value":67706,"color":"#e98125"},{"label":"Objective-C","value":36344,"color":"#cb2121"},{"label":"Shell","value":28561,"color":"#830909"},{"label":"Cobol","value":24131,"color":"#923e99"},{"label":"C#","value":100,"color":"#ae83d5"},{"label":"Coldfusion","value":68,"color":"#bf273e"},{"label":"Fortran","value":218812,"color":"#ce2aeb"},{"label":"Coffeescript","value":157618,"color":"#bca44a"},{"label":"Node","value":114384,"color":"#618d1b"},{"label":"Basic","value":95002,"color":"#1ee67b"},{"label":"Cola","value":36344,"color":"#b0ec44"},{"label":"Perl","value":32170,"color":"#a4a0c9"},{"label":"Dart","value":28561,"color":"#322849"},{"label":"Go","value":264131,"color":"#86f71a"},{"label":"Groovy","value":218812,"color":"#d1c87f"},{"label":"Processing","value":157618,"color":"#7d9058"},{"label":"Smalltalk","value":114384,"color":"#44b9b0"},{"label":"Scala","value":95002,"color":"#7c37c0"},{"label":"Visual Basic","value":78327,"color":"#cc9fb1"},{"label":"Scheme","value":67706,"color":"#e65414"},{"label":"Rust","value":36344,"color":"#8b6834"},{"label":"FoxPro","value":32170,"color":"#248838"}]},"labels":{"outer":{"pieDistance":32},"inner":{"hideWhenLessThanPercentage":3},"mainLabel":{"fontSize":11},"percentage":{"color":"#ffffff","decimalPlaces":0},"value":{"color":"#adadad","fontSize":11},"lines":{"enabled":true},"truncation":{"enabled":true}},"effects":{"pullOutSegmentOnClick":{"effect":"linear","speed":400,"size":8}},"misc":{"gradient":{"enabled":true,"percentage":100}}});
 }
 
 
