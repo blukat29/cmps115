8897a774bfd74b52d92628d5bf046f1e3f31ec0d
Seunghyo Kang <blueonion24@gmail.com>
2015-08-26 23:06:58 -0700

    Correct day of week order

--- app/scripts/analysis.js
+++ app/scripts/analysis.js
@@ -72,7 +72,7 @@ function drawDayHourHeatMap (data) {
     legendElementWidth = gridSize*2,
     buckets = 9,
     colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
-    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     times = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"];
 
 
@@ -95,7 +95,10 @@ function drawDayHourHeatMap (data) {
     .attr("y", function (d, i) { return i * gridSize; })
     .style("text-anchor", "end")
     .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-    .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+    .attr("class", function (d, i) {
+		if (i == 0) return "dayLabel mono axis axis-sun";
+		else if (i == 6) return "dayLabel mono axis axis-sat";
+		return "dayLabel mono axis axis-workweek";});
 
   var timeLabels = svg.selectAll(".timeLabel")
     .data(times)
@@ -105,7 +108,7 @@ function drawDayHourHeatMap (data) {
     .attr("y", 0)
     .style("text-anchor", "middle")
     .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-    .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+    .attr("class", function(d, i) { return (!(i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
 
   var heatMap = svg.selectAll(".hour")
     .data(data)
